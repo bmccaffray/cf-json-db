61771b151da27599a37a0d9b7e17b4076d80cf3a
Brett McCaffray <Brett@bmp.local>
2014-08-21 10:37:54 -0700

    edit with req body instead of req.params

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
 });
 
 app.post('/:jsonInput', function(req, res){
-	var jsonStr = JSON.stringify("{'jsonInput': " + req.params.jsonInput + "}");
+	var jsonStr = JSON.stringify("{'jsonInput': " + req.body + "}");
 	fs.writeFileSync('./output/jsonInput.json', jsonStr);
 });
 
